52556052472c5962c71d5d62c8144f6c7abc707a
meteor <tjdans34568@gmail.com>
2023-01-13 01:52:43 +0900

    ..

---
+++ src/practice/P3.java
@@ -0,0 +1,16 @@
+package practice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 구간 합부터는 다시 한번 씩 풀어봐야 할듯
+public class P3 {
+    public static void main(String[] args) throws IOException {
+
+
+    }
+
+}
+
