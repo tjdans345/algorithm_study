12790e8fee43b411d9a672a62780f16f5e998f19
meteor <tjdans34568@gmail.com>
2023-01-13 01:45:33 +0900

    ..

--- src/practice/P1.java
+++ src/practice/P1.java
@@ -1,9 +1,44 @@
 package practice;
 
+
+import java.util.Scanner;
+
 public class P1 {
 
+    /**
+     * 평균 값 구하기
+     * @param args args
+     */
     public static void main(String[] args) {
 
+        System.out.println("시험 과목 개수를 입력해주세요. ");
+        Scanner scanner = new Scanner(System.in);
+
+        int subjectCount = scanner.nextInt();
+        int[] subjectScoreArray = new int[subjectCount];
+
+        for(int i=0; i < subjectCount; i++) {
+            System.out.println(i + "번 째 과목의 점수를 입력해주세요.");
+            subjectScoreArray[i] = scanner.nextInt();
+        }
+
+        int sum = 0;
+        int max = 0;
+
+        for (int i=0; i < subjectCount; i++) {
+
+            // 내장 라이브러리 사용 했을 때
+//            max = Math.max(subjectScoreArray[i], max);
+            max = (subjectScoreArray[i] > max) ? subjectScoreArray[i] : max;
+
+            sum += subjectScoreArray[i];
+
+        }
+        System.out.println("최대 값 : " + max);
+        System.out.println("총합 : " + sum);
+
+
+        System.out.println("시험 평균은 " + sum * 100.0 / max / subjectCount );
 
     }
 }
--- src/practice/P2.java
+++ src/practice/P2.java
@@ -1,4 +1,32 @@
 package practice;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 구간 합부터는 다시 한번 씩 풀어봐야 할듯
+
 public class P2 {
-}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(stringTokenizer.nextToken());
+        int M = Integer.parseInt(stringTokenizer.nextToken());
+        long[] S = new long[N+1];
+        stringTokenizer = new StringTokenizer(br.readLine());
+
+        for(int i = 1; i <= N; i++) {
+            S[i] = S[i-1] + Integer.parseInt(stringTokenizer.nextToken());
+        }
+
+        for(int q = 0; q < M; q++) {
+            stringTokenizer = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(stringTokenizer.nextToken());
+            int j = Integer.parseInt(stringTokenizer.nextToken());
+
+            System.out.println(S[j] - S[i-1]);
+        }
+    }
+    }
